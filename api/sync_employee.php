d1ea54696a4a32d9c2a25d40280c486c09ba7c33
rKaushalya <ravinduk56@gmail.com>
2025-01-07 12:11:42 +0530

    sync emp data

---
+++ api/sync_employee.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include('../config.php');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+// Retrieve the transaction ID from the POST data
+
+
+if (!isset($_POST['emp_id'])) {
+    echo json_encode(array("message" => "Error: Missing parameters."));
+    exit();
+}else{
+    $id = $_POST['emp_id'];
+    try {
+        // get the branch id of given mpo
+        $result = query("SELECT branch_id FROM employee WHERE id = '$id' ",'../');
+        for ($i = 0; $row = $result->fetch(); $i++) {
+             $branch_id = $row['branch_id'];
+            }
+        // Prepare and execute the SQL query
+        $result = query("SELECT id, name, des FROM employee WHERE branch_id = '$branch_id' ",'../');
+    
+        // Fetch the results and create an array
+        $result_array = array();
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $result_array[] = $row;
+        }
+    
+        // Encode the array into JSON and output it
+        echo json_encode($result_array);
+    } catch (PDOException $e) {
+        echo json_encode(array("message" => "Error: " . $e->getMessage()));
+    }
+}
+
